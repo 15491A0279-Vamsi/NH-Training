3c2f10dbab898c4320fea9d7e0e05096f2b0d83e
15491A0279-Vamsi <m.vamsi75@gmail.com>
2022-02-13 17:07:59 +0530

    Day 15 Submission

---
+++ Day 15 Morning Assignment/Day15Project1/Day15Project1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Copy("E:\\Files\\Hello.txt", "E:\\Server\\Hello.txt");
+            Console.WriteLine("File Copy Done");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day 15 Morning Assignment/Day15Project2/Day15Project2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("E:\\Files\\Hello.txt",true);
+            sw.WriteLine("Nations Benefits => NB Healthcare Technologies");
+            sw.Close();
+
+            Console.WriteLine("Written in File Done");
+            Console.WriteLine("Append in a file is Done");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
