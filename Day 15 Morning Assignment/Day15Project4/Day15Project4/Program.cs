33b92fda087865061f00918eb09418b89f3b9bd5
15491A0279-Vamsi <m.vamsi75@gmail.com>
2022-02-13 17:17:42 +0530

    Day 15 Submission

---
+++ Day 15 Morning Assignment/Day15Project4/Day15Project4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("File Created Time:");
+            Console.WriteLine(File.GetCreationTime("E:\\Files\\Hello.txt"));
+            StreamReader sr = new StreamReader("E:\\Files\\Hello.txt");
+            string data = sr.ReadLine();
+
+            while (data != null)
+            {
+                Console.WriteLine(data);
+                data = sr.ReadLine();
+            }
+            Console.WriteLine("/************** File Reading Done ******************/");
+            Console.ReadLine();
+
+        }
+    }
+}
