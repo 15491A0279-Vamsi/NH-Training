769ce47fe40e91686463810c91aebba6e36f637b
15491A0279-Vamsi <m.vamsi75@gmail.com>
2022-02-15 17:43:28 +0530

    Day 17 Submission

---
+++ Day 17 Morning Assignment/Day17Project2/PublicLibrary/PublicLibraryDerivedClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VamsiKrishnaLibrary;
+
+namespace PublicLibrary
+{
+    public class PublicLibraryDerivedClass : BaseClass
+    {
+        public void PublicLibraryDerivedClassMethod()
+        {
+            a = 10;
+            //b = 20;           private variable can't access here
+            c = 30;
+            //d = 40;           internal variable can't access here
+            e = 50;
+        }
+    }
+    public class PublicLibraryOtherClass
+    {
+        public void PublicLibraryOtherClassMethod()
+        {
+            BaseClass b = new BaseClass();
+            b.a = 10;
+            //b.b = 20;   private variable can't access here
+            //b.c = 30;     Protected variable can't access here
+            //b.d = 40;     internal variable can't access here
+            //b.e = 50;        protected internal can't access here
+        }
+    }
+
+}
---
+++ Day 17 Morning Assignment/Day17Project2/VamsiKrishnaLibrary/BaseClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VamsiKrishnaLibrary
+{
+    public class BaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+
+        public void BaseClassMethod()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+            d = 40;
+            e = 50;
+        }
+
+
+
+    }
+    public class DerivedClass : BaseClass
+    {
+        public void DerivedClassMethod()
+        {
+            a = 10;
+            //b = 20;                  //Private variable can't access here
+            c = 30;
+            d = 40;
+            e = 50;
+        }
+    }
+    public class OtherClass
+    {
+        public void OtherClassMethod()
+        {
+            BaseClass b = new BaseClass();
+            b.a = 10;
+            //b.b = 20;     Private variable can't access here
+            //b.c = 30;     protected variable can't acess here
+            b.d = 40;
+            b.e = 50;
+        }
+    }
+}
