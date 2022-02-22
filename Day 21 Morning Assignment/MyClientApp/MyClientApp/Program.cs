17ce8cf7b126df90326e8760bb2bbf94fe848e83
15491A0279-Vamsi <m.vamsi75@gmail.com>
2022-02-22 12:45:19 +0530

    Day 21 Submission

---
+++ Day 21 Morning Assignment/MyClientApp/MyClientApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClientApp.ServiceReference1;
+
+namespace MyClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlgebraSoapClient obj = new AlgebraSoapClient();
+
+            Console.WriteLine(obj.Factorial(5));
+            Console.WriteLine(obj.Div(12,6));
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 21 Morning Assignment/WindowsApp/WindowsApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsApp.ServiceReference1;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            AlgebraSoapClient obj = new AlgebraSoapClient();
+            textBox2.Text = obj.Factorial(n).ToString();
+
+
+
+        }
+    }
+}
